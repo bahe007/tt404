d7f02a4ecc3963206fa2f43fd0377e5aedcc365a
Bastin Heinlein <bastian.heinlein@live.de>
2019-02-14 20:33:27 +0100

    Initial Commit
    Adds the readme file and the program.

---
+++ readme.md
@@ -0,0 +1,79 @@
+# tt404
+## Abstract
+tt404 is supposed to find faulty links on a website by crawling through it. It's able to write the results into a csv-file, to make the results easy to use and analyse. 
+## Project Motivation
+Most websites don't have a strict hierarchical navigation, instead there are links to other pages or even other websites within the article texts. This is especially critical, because it makes updating a website so difficult: If pages are moved to different urls, it can be quite hard to keep up with all the 404 errors. 
+
+Links to websites maintained by other companies are even worse: These websites can be deleted, altered or whatsoever, without letting the websites that link to them know. 
+
+## How It Works
+Like stated at the beginning, tt404 is a simple web crawler crawling through a given website. To start it, you give a base URL where it should begin. It searches for all links on that page and visits the referred pages. If the status code is not 200, it saves the link as faulty. If the referred website isn't the same top level domain as of the base URL, it just checks the directly linked site. If the referred website is the same top level domain, it visits all links on that page, too. 
+
+If it has visited all sites it could find, it saves the results into a csv file. 
+
+### Usage
+You can run the script directly from the terminal via Python 3.
+
+Valid arguments are: 
+- ```base_url``` required, URL at which the crawler should start
+- ```--output_csv``` optional, path for the output file, default: "tt404_analysis.csv"
+- ```--politeness``` optional, time between two requests in seconds, default: 0.0s
+- ```--ignore_urls``` optional, list of urls that should not be visited
+- ```--ignore_html_classes``` optional, list of class names for HTML tags whose content shall be ignored (except on the base url)
+- ```--ignore_html_ids``` optional, list of id names for HTML tags whose content shall be ignored (except on the base url)
+
+Example: 
+``` 
+python tt404.py https://example.com --output_csv path_to_file.csv --politeness 5.0
+```
+
+### CSV Format
+All csv files have the exact same format: The first line consists only of the given base URL. The second line shows what data is in each column and the following lines include the data. This should be a good trade-off between a human readable and a machine readable format. 
+
+Example: 
+```
+https://example.com 
+Origin, Label, Link, Status
+https://example.com/origin, Button-Label, Referred URL, Returned Status Code
+```
+
+If there are chilf HTML tags in the ```<a>```-Tag, the "Label"-field for the specific link will be empty, otherwise it will display what the button says to help identify the error more easily. 
+
+### Features
+tt404 can ignore certain parts (e. g. footer or navigation) of all pages visited except on the base URL in order to prevent recognizing the same mistake on every page. To allow this, provide the ```class``` or ```id``` tag. 
+
+It's also smart enough to recognize if a certain page has already been visited in order to be faster while crawling the website and prevent loops. 
+
+A small neat features it the ability to choose politeness. You can set a minimum time that tt404 should wait between every url request. 
+
+Last but not least, you can provide certain URLs that shouldn't be visited at all, for example if you have a calendar implementation that has a single web page for every month which would result in many unnecessary url calls. 
+
+### Requirements 
+- Python 3
+- Beautiful Soup
+- requests
+- argparse
+- csv (which I may remove and use an own csv-implementation based on the basic ```open()``` command)
+
+# License
+[Fair Source License, version 0.9](https://fair.io/v0.9.txt)
+
+Copyright © 2019 Bastian Heinlein (two things)
+
+Licensor: Bastian Heinlein
+
+Software: tt404 v1.0 
+
+Use Limitation: 5 users
+
+License Grant. Licensor hereby grants to each recipient of the Software (“you”) a non-exclusive, non-transferable, royalty-free and fully-paid-up license, under all of the Licensor’s copyright and patent rights, to use, copy, distribute, prepare derivative works of, publicly perform and display the Software, subject to the Use Limitation and the conditions set forth below.
+
+Use Limitation. The license granted above allows use by up to the number of users per entity set forth above (the “Use Limitation”). For determining the number of users, “you” includes all affiliates, meaning legal entities controlling, controlled by, or under common control with you. If you exceed the Use Limitation, your use is subject to payment of Licensor’s then-current list price for licenses.
+
+Conditions. Redistribution in source code or other forms must include a copy of this license document to be provided in a reasonable manner. Any redistribution of the Software is only allowed subject to this license.
+
+Trademarks. This license does not grant you any right in the trademarks, service marks, brand names or logos of Licensor.
+
+DISCLAIMER. THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OR CONDITION, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. LICENSORS HEREBY DISCLAIM ALL LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE.
+
+Termination. If you violate the terms of this license, your rights will terminate automatically and will not be reinstated without the prior written consent of Licensor. Any such termination will not affect the right of others who may have received copies of the Software from you.
---
+++ tt404.py
@@ -0,0 +1,235 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+import argparse
+import time 
+
+class Crawler:
+    
+    def __init__(self, base_url:str, output_file:str="tt404_analysis.csv", politeness=0.0, ignore_urls=None, ignore_classes=None, ignore_ids=None):
+        """
+
+        :param start_site:
+        """
+        if base_url[len(base_url)-1] != "/":
+            base_url = base_url + "/"
+
+        self.base_url = base_url
+        self.output_file = output_file 
+
+        self.politeness = politeness
+
+        self.urls_to_ignore = ignore_urls
+
+        self.classes_to_ignore = ignore_classes
+        self.ids_to_ignore = ignore_ids
+
+        self.broken_sites = list() # List Entry Format: (origin, label, target, status)
+        self.already_visited = list() # simple string list with all sites that are alreay visited and returned status code 200
+        self.todo = list() # List Entry Format: (origin, label, target)
+
+    def crawl(self):
+        """
+        Main method used to crawl through the website. 
+        :return: self.broken_sites
+        """
+        self.todo.append(("", "", self.base_url))
+        while len(self.todo) > 0:
+            origin, label, target = self.todo[0]
+            del self.todo[0]
+            target_url = self.normalize_link(target)
+            print(target_url)
+            if self.is_url_broken(target_url): # target is a known faulty page
+                self.broken_sites.append((origin, label, target_url, self.broken_url_status_code(target_url)))
+                continue
+            elif self.is_url_already_visited(target_url) or self.should_url_be_ignored(target_url): # target is already visited or should be ignored anyways
+                continue
+            else: # Seite wurde noch nicht besucht
+                content, status_code = self.visit(target_url)
+                if status_code != 200:
+                    self.broken_sites.append((origin, label, target_url, status_code))
+                else:
+                    self.already_visited.append(target_url)
+                    if self.is_link_to_tld(target_url):
+                        is_start_page = False 
+                        if target_url == self.base_url: 
+                            is_start_page = True 
+                        links = self.get_links(content, is_base_url=is_start_page)
+                        for link in links:
+                            self.todo.append((target_url, link[0], link[1]))
+
+
+
+
+    def visit(self, url:str):
+        """
+        Calls a given link and returns the site content and status code. 
+        :param link: normalisierter Link zur aufzurufenden Seite.
+        :return: (content, status)
+        """
+        time.sleep(self.politeness)
+        response = requests.get(url)
+        if response.status_code == 200:
+            return (response.content, response.status_code)
+        else:
+            return ("", response.status_code)
+
+    def is_link_to_tld(self, url: str):
+        """
+        Checks if a normalized link is part of the same top level domain as the base url. 
+        :param url:
+        :return:
+        """
+        if len(url) >= len(self.base_url):
+            if url[:len(self.base_url)] == self.base_url:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def get_links(self, content: str, is_base_url:bool=False):
+        """
+        Returns a list with all links that could be found in a given content string that shouldn't be ignored. 
+
+        List format: (label, link)
+        :param content:
+        :return:
+        """
+        return_links = list()
+        soup = BeautifulSoup(content, "html.parser")
+
+        if self.classes_to_ignore is not None and is_base_url is not True: 
+            for css_class in self.classes_to_ignore:
+                results = soup.select(".{}".format(css_class))
+                if results is not None:
+                    for result in results:
+                        result.decompose()
+        
+        if self.ids_to_ignore is not None and is_base_url is not True: 
+            for css_id in self.ids_to_ignore:
+                results = soup.select("#{}".format(css_id))
+                if results is not None:
+                    for result in results:
+                        result.decompose()
+
+        all_links = soup.find_all("a")
+        for link in all_links:
+            if link.has_attr("href"):
+                url = link["href"]
+                if len(url) > 0:
+                    if url[0] == "/":
+                        if link.string != None:
+                            return_links.append((link.string, url))
+                        else:
+                            return_links.append(("", url))
+                        continue
+                if len(url) > 8:
+                    if url[:8] == "https://":
+                        if link.string != None:
+                            return_links.append((link.string, url))
+                        else:
+                            return_links.append(("", url))
+                        continue
+
+        return return_links
+
+    def normalize_link(self, link:str):
+        """
+        Normalizes the link in a way, so that you can paste it directly in your browser and reach the site. 
+        :param link:
+        :return:str
+        """
+        if len(link) > 8:
+            if link[:8] == "https://":
+                return link
+            elif link[:7] == "http://":
+                return link
+        if len(link) > 1:
+            if link[0] == "/":
+                link = link[1:]
+        elif link == "/":
+            return self.base_url
+        return self.base_url+link
+
+    def is_url_broken(self, url: str):
+        """
+        Determines, if the url is known as broken. 
+        :param url:
+        :return: Boolean
+        """
+        for element in self.broken_sites:
+            if element[2] == url:
+                return True
+        return False
+
+    def broken_url_status_code(self, url: str):
+        """
+        Looks up the status code of an url, that is definitely broken. If it couldn't find it, it returns -1. 
+        :param url:
+        :return:
+        """
+        for element in self.broken_sites:
+            if element[2] == url:
+                return element[3]
+        return -1
+
+    def is_url_already_visited(self, url: str):
+        """
+        Determines, if the urls has already been visited. 
+        :param url:
+        :return: Boolean
+        """
+        if url in self.already_visited:
+            return True
+        else:
+            return False
+
+    def should_url_be_ignored(self, url: str): 
+        """
+        Determines, wether a given url is one of the urls that should be ignored
+        :param url: 
+        :return: Boolean
+        """
+        if self.urls_to_ignore == None: 
+            return False 
+        else: 
+            for ignore_url in self.urls_to_ignore:
+                if ignore_url in url: 
+                    return True 
+            return False
+
+    def safe_as_csv(self):
+        """
+        Writes the content of "self.broken_sites" to the initially specified output path without determining if there is already a file with the same name. 
+        :return: None
+        """
+        with open(self.output_file, "w") as csvfile:
+            writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow([self.base_url, "", "", ""])
+            writer.writerow(["Origin", "Label", "Link", "Status"])
+            for broken_site in self.broken_sites:
+                writer.writerow(broken_site)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Finds faulty links on your website. More information at: https://github.com/bahe007")
+    parser.add_argument('base_url', type=str,
+                        help='The base url where the crawler should start')
+    parser.add_argument('--output_csv', type=str, default="tt404_analysis.csv")
+    parser.add_argument('--politeness', type=float, default=0.0)
+    parser.add_argument("--ignore_urls", type=str, nargs="+")
+    parser.add_argument("--ignore_html_classes", type=str, nargs="+")
+    parser.add_argument("--ignore_html_ids", type=str, nargs="+")    
+
+    arguments = parser.parse_args()
+    
+    base_url = arguments.base_url 
+    output_csv = arguments.output_csv
+    politeness = arguments.politeness
+    ignore_urls = arguments.ignore_urls 
+    ignore_classes = arguments.ignore_html_classes
+    ignore_ids = arguments.ignore_html_ids
+
+    crawler = Crawler(base_url, output_file=output_csv, ignore_urls=ignore_urls, ignore_classes=ignore_classes, ignore_ids=ignore_ids, politeness=politeness)
+    crawler.crawl()
+    crawler.safe_as_csv()
